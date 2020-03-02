df5ddb359adbae2579d6e624589fa170cb7cb35c
GSGSS <342517845@QQ.COM>
2020-03-02 23:11:51 +0800

    add list.py

--- filter.py
+++ filter.py
@@ -1,4 +1,5 @@
-a=[1,2,3,4,5,6,7,8,9]
-b=5
-filter(lambda x: x<b,a)
-print(list(filter(lambda x: x<b,a)))
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+b = 5
+slict = filter(lambda x: x < b, a)
+print(type(slict))
+print(list(slict))
---
+++ list.py
@@ -0,0 +1,5 @@
+a_list=["abc","xyz"]
+a_list.append('x')
+print(a_list)
+a_list.remove('x')
+print(a_list)
