0fb4db39fdbb39ef2047a3e9695130d9d1d8eee2
GSGSS <342517845@QQ.COM>
2020-04-21 00:00:59 +0800

    add AI dir

---
+++ AIlearning/numpy_test.py
@@ -0,0 +1,17 @@
+import numpy as np
+arr1 = np.array([2, 3, 4])
+
+print(arr1)
+print(arr1.dtype)
+
+arr2 = np.array([1.3, 2.4, 5.3])
+print(arr2)
+print(arr2.dtype)
+
+print(arr1+arr2)
+
+print(np.zeros([3, 5]))
+
+print(np.ones((3, 5, 4)))
+
+print(np.empty((2, 3, 4)))
---
+++ AIlearning/pandas_test.py
@@ -0,0 +1,8 @@
+# from pandas import Series,DataFrame
+import pandas as pd
+
+obj = pd.Series([3, 4, 6, -1])
+
+print(obj)
+print(obj.values)
+print({'a': 1, 'b': 2})
